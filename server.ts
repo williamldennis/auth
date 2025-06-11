cbc76f5b49e5e001b6d75a4f6445dca09fd70cab
Will Dennis <williamldennis@gmail.com>
2025-06-11 10:49:35 -0400

    swagger docs working

--- server.ts
+++ server.ts
@@ -1,21 +1,80 @@
-import express from 'express'
 import cors from 'cors'
-
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
 const app = express()
-const PORT = 3001
+const PORT = 3000
 
 app.use(cors())
-app.use(express())
+app.use(express.json())
+
+//configure swagger
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: "WD Auth API Docs",
+            version: '1.0.0',
+            description: 'One of the best APIs honestly'
+        },
+        servers: [
+            { url: 'http://localhost:3000'}
+        ]
+    },
+    apis: ['./server.ts']
+}
+
+//generate swagger spec
+const swaggerDocs = swaggerJsdoc(swaggerOptions)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+
+/**
+ * @swagger
+ * /api/public:
+ *   get:
+ *     summary: Public information
+ *     description: All public information lives here
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content: 
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message: 
+ *                   type: string
+ *                   example: "This info is public"
+ */
 
 app.get('/api/public', (req, res) => {
     res.json({ message: "This is public information"}) 
 })
-
+/**
+ * @swagger
+ * /api/protected:
+ *   get:
+ *     summary: Protected information
+ *     description: All protected information lives here
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content: 
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message: 
+ *                   type: string
+ *                   example: "This info is protected"
+ */
 app.get('/api/protected', (req, res) => {
     res.json({ message: "Only admins should be able to see this"}) 
 })
 
 app.listen(PORT, () => {
     console.log(`server running on ${PORT}`);
+    console.log(`API docs avail at ${PORT}/api-docs`)
     
 })
